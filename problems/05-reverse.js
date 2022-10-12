4c53a5535c76776b8ef32392c740ba8d88e8f514
Kourani <76729542m@gmail.com>
2022-10-12 18:28:04 -0400

    First Commit

---
+++ problems/05-reverse.js
@@ -0,0 +1,40 @@
+/***********************************************************************
+Write a recursive function reverse(string) that takes in a string and returns
+it reversed.
+
+Examples:
+
+reverse("house"); // "esuoh"
+reverse("dog"); // "god"
+reverse("atom"); // "mota"
+reverse("q"); // "q"
+reverse("id"); // "di"
+reverse(""); // ""
+***********************************************************************/
+
+
+function reverse(str) {
+  // Your code here
+
+  if(str.length === 0)
+  {
+    return ''
+  }
+
+return  reverse(str.slice(1)) + str[0] 
+}
+
+console.log(reverse("house")); // "esuoh"
+console.log(reverse("dog")); // "god"
+reverse("atom"); // "mota"
+reverse("q"); // "q"
+reverse("id"); // "di"
+reverse(""); // ""
+
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = reverse;
+} catch (e) {
+  module.exports = null;
+}
